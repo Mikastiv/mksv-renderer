1eb1070d46554759537d30eb98edb372c050e1cc
Mikastiv <mikastiv@outlook.com>
2023-05-15 20:40:24 -0400

    more warnings

--- mksv_renderer/inc/mksv/mksv_wrl.hpp
+++ mksv_renderer/inc/mksv/mksv_wrl.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#pragma warning( push )
+#pragma warning( disable : 4265 )
+
 #include <wrl.h>
+
+#pragma warning( pop )
 
 template <typename T>
 using ComPtr = Microsoft::WRL::ComPtr<T>;
